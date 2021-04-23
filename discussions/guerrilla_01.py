5407ffdbde3fda559ae6a07eabf29f4c0a565405
Hett-XY-14 <67879862+Hett-XY-14@users.noreply.github.com>
2021-04-23 14:41:30 -0500

    guerrilla_01 excercises

--- discussions/guerrilla_01.py
+++ discussions/guerrilla_01.py
@@ -33,3 +33,64 @@ list1 = [2, 3, 5, 6, 9]
 list2 = [1, 4, 7, 8, 10]
 
 print(merge(list1, list2))
+
+# Mario needs to jump over a sequence of Piranha plants, represented as a string of spaces (no plant) and P's (plant!). He only moves forward, and he can either step (move forward one space) or jump (move forward two spaces) from each position. How many different ways can Mario traverse a level without stepping or jumping into a Piranha plant? Assume that every level begins with a space (where Mario starts) and ends with a space (where Mario must end up):
+
+
+def mario_number(level):
+    """Return the number of ways that Mario can perform a sequence of steps or jumps to reach the end of the level without ever landing in Piranha plant. Assume that every level begins and ends whit a space.
+
+    >>> mario_number(' P P ') # jump , jump
+    1
+    >>> mario_number(' P P  ') # jump, jum, step
+    1
+    >>> mario_number('  P P ') # step, jump, jump
+    1
+    >>> mario_number('   P P ') # step, step, jump, jump or jump, jump, jump
+    2
+    >>> mario_number(' P PP ') # Mario cannot jump two plants
+    0
+    >>> mario_number('    ') # step, jump; jump, step; step, step, step
+    3
+    >>> mario_number('    P    ')
+    9
+    >>> mario_number('   P    P P   P  P P    P     P ')
+    180
+    """
+    total = 0
+    if level[0] == 'P':
+        return 0
+    if len(level) == 1 and level[0] == ' ':
+        return 1
+    if len(level) > 2:
+        return mario_number(level[1:]) + mario_number(level[2:])
+    else:
+        return mario_number(level[1:])
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
+# H  i  g  h  e  r    O  r  d  e  r    F  u  n  c  t  i  o  n  s
+
+""" Express the following lambda expression using a def statement,
+and the def statement using a lambda expressión."""
+
+
+def pow(x, y): return x ** y
+
+
+def foo(x):
+    def f(y):
+        def g(z):
+            return x + y * z
+        return g
+    return f
+
+
+def pow(x, y):
+    return x ** y
+
+
+def foo(x): return lambda y: lambda z: x + y * z
