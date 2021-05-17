d54c0a2ca916ce47624b777535d73096a4b841c1
Hett-XY-14 <67879862+Hett-XY-14@users.noreply.github.com>
2021-05-17 17:32:44 -0500

    Project 02 'typing test'; problems 06 and 07 completed

--- projects/typing_test/typing_test.py
+++ projects/typing_test/typing_test.py
@@ -100,29 +100,81 @@ print(autocorrect("goje", words_list, swap_score))
 
 
 def score_function(word1, word2):
-    """A score_function that computes the edit distance between word1 and word2."""
+    """A score_function that computes the edit distance between word1 and word2.
+    >>> score_function("tesng", "testing")
+    2
+    >>> score_function("rlogcul", "logical")
+    3
+    >>> score_function("misspelled", "spelling")
+    6
+    """
 
-    if ______________:  # Fill in the condition
+    if word1 == "" or word2 == "":  # Fill in the condition
         # BEGIN Q6
-        "*** YOUR CODE HERE ***"
+        if (word1):
+            return len(word1)
+        if (word2):
+            return len(word2)
+        return 0
         # END Q6
 
-    elif ___________:  # Feel free to remove or add additional cases
+    elif word1[0] == word2[0]:  # Feel free to remove or add additional cases
         # BEGIN Q6
-        "*** YOUR CODE HERE ***"
+        return score_function(word1[1:], word2[1:])
         # END Q6
 
     else:
-        add_char = ______________  # Fill in these lines
-        remove_char = ______________
-        substitute_char = ______________
+        # Fill in these lines
+        add_char = score_function(word2[0] + word1, word2)
+        remove_char = score_function(word1[1:], word2)
+        substitute_char = score_function(word2[0] + word1[1:], word2)
         # BEGIN Q6
-        "*** YOUR CODE HERE ***"
+        return 1 + min(add_char, remove_char, substitute_char)
         # END Q6
 
 
+print(score_function("tesng", "testing"))
+print(score_function("rlogcul", "logical"))
+print(score_function("misspelled", "spelling"))
 KEY_DISTANCES = get_key_distances()
+print(KEY_DISTANCES["q", "t"])
+print(KEY_DISTANCES["q", "e"])
 
 # BEGIN Q7-8
+
+
+def score_function_accurate(word1, word2):
+    if word1 == "":
+        if (word2):
+            return float(len(word2))
+        return 0.0
+
+    elif word2 == "":
+        if (word1):
+            return float(len(word1))
+        return 0.0
+
+    elif word1[0] == word2[0]:
+        return score_function_accurate(word1[1:], word2[1:])
+
+    else:
+        add_char = 1 + score_function_accurate(word2[0] + word1, word2)
+        remove_char = 1 + score_function_accurate(word1[1:], word2)
+        substitute_char = 1 + score_function_accurate(
+            word2[0] + word1[1:], word2) + KEY_DISTANCES[word1[0], word2[0]]
+        return min(add_char, remove_char, substitute_char)
+
+
+print(score_function_accurate("tesng", "testing"))
+print(score_function_accurate("rlogculu", "logicala"))
+print(score_function_accurate("misspelled", "spelling"))
+print(score_function_accurate("cuetara", "cuotoro"))
+print(score_function_accurate("cuetara", "cuotore"))
+print(score_function_accurate("amaranto", "amaramsta"))
+
+# print(KEY_DISTANCES["u", "a"])
+# print(KEY_DISTANCES["d", "g"])
+# print(KEY_DISTANCES["d", "g"]+KEY_DISTANCES["e", "n"])
+# print(KEY_DISTANCES["d", "g"]+KEY_DISTANCES["e", "n"]+6)
 "*** YOUR CODE HERE ***"
 # END Q7-8
